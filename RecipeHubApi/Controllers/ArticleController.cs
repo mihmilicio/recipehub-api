4d937e0364e4aa8429e75da94e2b4ea518c4c2e4
lunaboona <hyaluna@gmail.com>
2021-09-27 01:05:51 -0300

    update article<->recipe relationships in article update

--- RecipeHubApi/Controllers/ArticleController.cs
+++ RecipeHubApi/Controllers/ArticleController.cs
@@ -65,7 +65,7 @@ namespace RecipeHubApi.Controllers
                 return UnprocessableEntity();
             }
 
-            var originalArticle = _context.Article.Find(articleId);
+            var originalArticle = GetById(articleId);
             if (originalArticle == null)
             {
                 return NotFound();
@@ -76,7 +76,26 @@ namespace RecipeHubApi.Controllers
                 return Unauthorized();
             }
             
-            _context.Entry(originalArticle).State = EntityState.Detached;
+            if (article.ArticleRecipes is null || article.ArticleRecipes.Count == 0)
+            {
+                return UnprocessableEntity();
+            }
+            
+            SetStates(originalArticle, EntityState.Detached);
+
+            var originalArticleRecipes = originalArticle.ArticleRecipes.Select(i => i.Id).ToList();
+            var newArticleRecipes = article.ArticleRecipes.Select(i => i.Id).ToList();
+
+            var articleRecipesToDelete = originalArticleRecipes.Except(newArticleRecipes).ToList();
+            var articleRecipesToAdd = newArticleRecipes.Except(originalArticleRecipes).ToList();
+
+            articleRecipesToDelete.ForEach(articleRecipeId => _context.Entry(_context.ArticleRecipe.Find(articleRecipeId)).State = EntityState.Deleted);
+            articleRecipesToAdd.ForEach(articleRecipeId =>
+            {
+                var match = article.ArticleRecipes.ToList().Find(x => x.Id == articleRecipeId);
+                if (match != null)
+                    _context.Entry(match).State = EntityState.Added;
+            });
             
             try
             {
@@ -101,6 +120,15 @@ namespace RecipeHubApi.Controllers
             return Ok(article);
         }
         
+        private void SetStates(Article article, EntityState state)
+        {
+            _context.Entry(article).State = state;
+            foreach (var articleRecipe in article.ArticleRecipes)
+            {
+                _context.Entry(articleRecipe).State = state;
+            }
+        }
+        
         [HttpGet]
         public List<Article> GetAll()
         {
@@ -112,15 +140,21 @@ namespace RecipeHubApi.Controllers
             return articles.Select(IncludeRecipes).ToList();
         }
         
-        [HttpGet]
-        [Route("{articleId}")]
-        public Article GetById([FromRoute] string articleId)
+        private Article GetById(string articleId)
         {
             var article = _context.Article
                 .Include(a => a.User)
                 .Include(a => a.ArticleRecipes)
                 .FirstOrDefault(a => a.Id == articleId);
             return IncludeRecipes(article);
+        }
+        
+        [HttpGet]
+        [Route("{articleId}")]
+        public IActionResult GetByIdRequest([FromRoute] string articleId)
+        {
+            var article = GetById(articleId);
+            return article is not null ? Ok(article) : NotFound();
         }
 
         private Article IncludeRecipes(Article article)
